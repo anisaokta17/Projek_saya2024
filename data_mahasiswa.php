af9e7edc968ee09ea119c1faa8cfe4f0f76e69df
anisaokta17 <168271286+anisaokta17@users.noreply.github.com>
2024-04-27 17:14:15 +0700

    Create data_mahasiswa.php

---
+++ data_mahasiswa.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "<h1>Data Mahasiswa</h1>";
+echo "NIM = 22110027";
+echo "Nama Lengkap = Anisa Okta Fadilah";
+echo "Tempat Tanggal Lahir = Pekanbaru";
+echo "Tanggal Lahir = 17 Oktober 2002";
+echo "Alamat = Dusun II Rimbo Panjang";
+echo "Kecamatan = Tambang";
+echo "Kabupaten = Kampar";
+echo "Kelas = 22MI B";
+
+?>
